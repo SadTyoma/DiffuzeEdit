1c9e673d85e4de2c094f241d6e1f199d95cbf92c
Sad Tyoma <artyomshuneyko@gmail.com>
2022-11-24 22:03:14 +0300

    Initial Commit

---
+++ term_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  term_project
+//
+//  Created by Artem Shuneyko on 24.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
